95ae9e55c368bab358b1ecc82291fef31ee03e24
rahiq <ico3436412@gmail.com>
2026-07-02 16:14:06 +0200

    fix logic errors and improve time display

--- clock program.cpp
+++ clock program.cpp
@@ -5,28 +5,38 @@ using namespace std;
 int main()
 {
 	int hrs, min, sec;
-	cout<<"Hours ";
+	cout<<"Enter hours (0-23): ";
 	cin>>hrs;
-	cout<<"Minutes ";
+	cout<<"Enter minutes (0-59) ";
 	cin>>min;
-	cout<<"Secondes ";
+	cout<<"Enter secondes (0-59) ";
 	cin>>sec;
+
+	if(sec < 0 || sec > 59) sec = 0;
+	if(min < 0 || min > 59) min = 0;
+	if(hrs < 0 || hrs > 23) hrs = 0;
+
 	while (1){
 		system("cls");
-		if(sec>59){
-			min++;
-			sec=0;
+
+		cout<< (hrs < 10 ? "0" : "") << hrs <<":"
+		    << (min < 10 ? "0" : "") << min <<":"
+			<< (sec < 10 ? "0" : "") << sec <<endl;
+
+		sec++;	
+		if(sec > 59){
+			sec = 0;
+			min++ ;
+		    if(min > 59){
+			   min = 0;
+			   hrs++ ;
+		       if(hrs > 23){
+			       hrs=0;
 		}
-		if(min>59){
-			hrs++;
-			min=0;
-		}
-		if(hrs<23){
-			hrs=0;
-		}
-		cout<<hrs<<":"<<min<<":"<<sec<<endl;
-		sec++;
-		Sleep(900);
 	}
-	return 0;
 }
+
+		
+Sleep(1000);
+}
+	return 0;
